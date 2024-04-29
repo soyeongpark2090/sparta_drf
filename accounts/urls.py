b1e54e0a85777d8bff4291db1439cca2acf00de4
soyeongpark2090 <syp4546@gmail.com>
2024-04-30 01:26:05 +0900

    로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import CustomUserCreate
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', CustomUserCreate.as_view(), name="create_user"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
