6778a653c455e47b90d8d17ee0b49be1412820a0
soyeongpark2090 <syp4546@gmail.com>
2024-04-30 01:09:04 +0900

    회원가입

---
+++ accounts/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class CustomAccountManger(BaseUserManager):
+    def create_superuser(self, email, user_name,password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+        return self.create_user(email, user_name, password, **other_fields)
+
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+    
+class NewUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    nickname = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+    birth = models.DateTimeField()
+    about = models.TextField(_('about'), max_length=500, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True) # False면 email을 확인하기 전까진 비활성화 상태
+
+    objects = CustomAccountManger() # 일반/슈퍼 user 모두 처리
+
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email', 'birth',] # 필수 항목
+
+    def __str__(self):
+        return self.user_name
