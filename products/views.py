474281adf08e6f16580fea48073cc252ddd4985a
soyeongpark2090 <syp4546@gmail.com>
2024-04-30 14:57:36 +0900

    products앱 등록, pillow설치, freeze

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Product
+from .serializers import ProductSerializer
 
 class ProductListAPIView(APIView):
     def post(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
