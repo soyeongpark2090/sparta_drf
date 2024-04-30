1f374e42b5a08aeeb579c6f88af8f69709e9135a
soyeongpark2090 <syp4546@gmail.com>
2024-04-30 16:37:00 +0900

    물품삭제

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ProductListAPIView.as_view()),
+    path("<int:productId>", views.ProductDetailAPIView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -15,3 +16,10 @@ class ProductListAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
         return Response(serializer.data)
+
+
+class ProductDetailAPIView(APIView):
+    def delete(self,request,productId):
+        product = get_object_or_404(Product,pk=productId)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
